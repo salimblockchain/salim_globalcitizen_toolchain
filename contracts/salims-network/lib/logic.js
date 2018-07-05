5cc50a85428d529eb80f6e5126b5550f0b3d2d7c
mohamskh <mohamskh@in.ibm.com>
2018-07-05 18:32:06 +0530

    ghghdg

--- contracts/salims-network/lib/logic.js
+++ contracts/salims-network/lib/logic.js
@@ -10,23 +10,23 @@ var NS = 'org.salim';
  * @transaction
  */
 async function sampleTransaction(tx) {
-  // Save the old value of the asset.
-  const oldValue = tx.asset.value;
+    // Save the old value of the asset.
+    const oldValue = tx.asset.value;
 
-  // Update the asset with the new value.
-  tx.asset.value = tx.newValue;
+    // Update the asset with the new value.
+    tx.asset.value = tx.newValue;
 
-  // Get the asset registry for the asset.
-  const assetRegistry = await getAssetRegistry('org.salim.SampleAsset');
-  // Update the asset in the asset registry.
-  await assetRegistry.update(tx.asset);
+    // Get the asset registry for the asset.
+    const assetRegistry = await getAssetRegistry('org.salim.SampleAsset');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.asset);
 
-  // Emit an event for the modified asset.
-  let event = getFactory().newEvent('org.salim', 'SampleEvent');
-  event.asset = tx.asset;
-  event.oldValue = oldValue;
-  event.newValue = tx.newValue;
-  emit(event);
+    // Emit an event for the modified asset.
+    let event = getFactory().newEvent('org.salim', 'SampleEvent');
+    event.asset = tx.asset;
+    event.oldValue = oldValue;
+    event.newValue = tx.newValue;
+    emit(event);
 }
 
 /**
